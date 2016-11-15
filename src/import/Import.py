ae28b94cebf452667e503415e8bdc56ce5cb8159
Christopher Moore <cjm96@arche.damtp.cam.ac.uk>
2016-11-15 13:00:26 +0000

    .

--- src/import/Import.py
+++ src/import/Import.py
@@ -1,1 +1,15 @@
-hello
+import numpy as np
+
+
+def LoadUliMode(filename):
+    dat = np.loadtxt(filename)
+    Time = dat[:,0]
+    Re   = dat[:,1]
+    Im   = dat[:,2]
+    return [Time, Re, Im]
+
+def LoadUliData():
+    LoadUliMode("/store/DAMTP/cjm96/public_html/Modes/Psi4_vars_l2_m0_rex02")
+
+
+
