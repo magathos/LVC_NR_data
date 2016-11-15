6928491700f760c58a9499c70503bb8b2e8922b5
Christopher Moore <cjm96@arche.damtp.cam.ac.uk>
2016-11-15 14:49:22 +0000

    .

--- src/ConvertDataToLSC.py
+++ src/ConvertDataToLSC.py
@@ -1,13 +1,21 @@
 import numpy as np
+import sys
+sys.path.insert(0, './export/')
+sys.path.insert(0, './import/')
 
 from NR_Import import *
 from NR_Export import *
 
 # Load Data
 input_path = "/store/DAMTP/cjm96/public_html/Modes/"
-data = LoadUliData(input_path)
+#data = LoadUliData(input_path)
 
 # Save Data
 output_path = "$HOME/Desktop/"
 output_file = "test.h5"
+
+data = np.zeros((100,2))
+data[:,0] = np.arange(0,10,0.1)
+data[:,1] = np.sin(data[:,0])
+
 SaveData(data, output_file, output_path)
--- src/export/NR_Export.py
+++ src/export/NR_Export.py
@@ -1,1 +1,11 @@
-hello
+import numpy as np
+import romspline
+
+def SaveData(data, output_file, output_path):
+    filename = output_path + output_file
+
+    spline = romspline.ReducedOrderSpline(data[:,0], data[:,1], verbose=False)
+
+    print spline(np.pi)
+
+    return 1
