3876cf8e4c2a0892e643d0da7340238a1edf876e
Christopher Moore <cjm96@arche.damtp.cam.ac.uk>
2016-11-15 14:20:47 +0000

    .

--- src/ConvertDataToLSC.py
+++ src/ConvertDataToLSC.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-
 # Load Data
-data = LoadUliData()
+input_path = "/store/DAMTP/cjm96/public_html/Modes/"
+data = LoadUliData(input_path)
 
 # Save Data
-SaveData(data)
+output_path = "$HOME/Desktop/"
+output_file = "test.h5"
+SaveData(data, output_file, output_path)
---
+++ src/export/NR_Export.py
@@ -0,0 +1,11 @@
+import numpy as np
+import romspline
+
+
+
+def SaveData(data, simname, path):
+
+    file = path + simname
+
+
+
