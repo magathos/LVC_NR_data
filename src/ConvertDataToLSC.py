57cc44f152f3bceb4656aeef90ac74983cfb1df2
Michalis Agathos <magathos@damtp.cam.ac.uk>
2016-11-25 17:48:14 +0000

    Added functions for file parsing

--- src/ConvertDataToLSC.py
+++ src/ConvertDataToLSC.py
@@ -11,32 +11,74 @@ sys.path.insert(0, './import/')
 from NR_Import import *
 from NR_Export import *
 
+def modesuptolmax(lmax):
+  res = []
+  for l in arange(lmax-1) +2:
+    for m in arange(2*l+1)-lmax:
+      res.append((l,m))
+  return set(res)
+
+def check_modelist(modelist, lmax=None):
+  '''modelist is list of (l,m) tuples. Check whether all modes up to lmax are included.'''
+  if lmax is None:
+    lmax = max(array(modelist)[:,0])
+  return len(set(modelist) - modesuptolmax(lmax))
+
 class NR_data:
 #  Class that hosts imported data from NR simulations
-  def __init__(self, nrtype, nrid, sourcedir, lmax=2):
+  def __init__(self, nrtype, nrid, sourcedir, lmax=None, rlist=None):
     self.lmax = lmax
     self.nrid = nrid
     self.nrtype = nrtype
     self.sourcedir = sourcedir
     self.Psi4modes = []
+    if lmax is None:
+      self.readmodelist()
+    else:
+      self.llist = arange(self.lmax-1) + 2
+      for l in self.llist:
+        mlist = arange(2*l+1) - l
+        modedict[l] = {}
+    if rlist is None:
+      self.readrlist()
 
-  def populate_modes(self, lmax):
+  def parse_params(self):
+    '''Read parameter file(s)'''
+    
+
+  def read_modelist(self):
+    '''Read list of modes available in source directory'''
+    modelist = []
+    loc = self.sourcedir
+        
+
+
+  def populate_modes(self, lmax, rlist):
     '''Populate modes with data for l<=lmax'''
     modedict = {}
-    llist = arange(self.lmax-1) + 2
+    if lmax is None:
+      print "Importing all available modes... not implemented yet, exiting..."
+      sys.exit(-1)
     for l in llist:
       mlist = arange(2*l+1) - l
       modedict[l] = {}
       for m in mlist:
-        thisNR_mode = NR_mode(l, m, self.nrtype, self.sourcedir)
-        thisNR_mode.load_data()
-        modedict[l][m] = thisNR_mode
+        for r in rlist:
+          thisNR_mode = NR_mode(l, m, r, self.nrtype, self.sourcedir)
+          thisNR_mode.load_data()
+          modedict[l][m] = thisNR_mode
         
     self.Psi4modes = modelist
 
     
   def Psi4mode(self, l, m):
     '''Get the mode (l,m)'''
+    if l not in self.Psi4modes.keys():
+      print "Mode l=",l,", m=",m," do not exist. Exiting..."
+      sys.exit(-1)
+    elif m not in self.Psi4modes[l].keys():
+      print "Mode l=",l,", m=",m," does not exist. Exiting..."
+      sys.exit(-1)
     return self.Psi4modes[l][m]
 
 
@@ -57,7 +99,7 @@ class NR_mode:
 
   def load_data(self, source=None):
     '''Write Psi4 time series data to mode'''
-    loadmodefunc = loadFuncDict[self.nrtype]
+    loadmodefunc = loadDataFDict[self.nrtype]
     nrtime, Psi4C = loadmodefunc(self.l, self.m, self.r, self.sourcedir)
     #sanity checks
     self.t = nrtime
@@ -67,22 +109,30 @@ class NR_mode:
 
 if __name__ == "__main__":
 
-parser.add_argument('-t', '--NRtype', type=str, dest='NRtype', required=True, nargs=1, choices=['Uli','GRChombo'], help='Type of NR data to be imported. Currently supported: {"Uli", "GRChombo"}')
-parser.add_argument('-m', '--mode', type=int, nargs=2)
+  parser.add_argument('-t', '--NRtype', type=str, dest='NRtype', required=True, nargs=1, choices=['Uli','GRChombo'], help='Type of NR data to be imported. Currently supported: {"Uli", "GRChombo"}')
+  parser.add_argument('-m', '--mode', type=int, nargs=2)
+  parser.add_argument('-l', '--lmax', type=int, nargs=1)
+  parser.add_argument('-i', '--inputdir', dest='inputdir', type=str, nargs=1)
+  parser.add_argument('-o', '--outputdir', dest='outputdir', type=str, nargs=1)
+  
+  # Hardcoded list of modes (FIXME)
+  modelist = [[2,2]]
 
+  NRtype = parser.NRtype
+  
+  # Load Data
+  input_path = parser.inputdir
+  #data = LoadUliData(input_path)
+  
+  
+  # Save Data
+  output_path = parser.outputdir
+  output_file = os.path.join(output_path,"test.h5")
+  
+  data = NR_data(NRtype, NRID, input_path, lmax)
 
-
-# Load Data
-input_path = "/store/DAMTP/cjm96/public_html/Modes/"
-#data = LoadUliData(input_path)
-
-
-# Save Data
-output_path = os.environ['HOME']+"/Desktop/"
-output_file = "test.h5"
-
-data = np.zeros((100,2))
-data[:,0] = np.arange(0,10,0.1)
-data[:,1] = np.sin(data[:,0])
-
-SaveData(data, output_file, output_path)
+  data = np.zeros((100,2))
+  data[:,0] = np.arange(0,10,0.1)
+  data[:,1] = np.sin(data[:,0])
+  
+  SaveData(data, output_file, output_path)
--- src/import/NR_Import.py
+++ src/import/NR_Import.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
-
-loadFuncDict = {'Uli':LoadUliData, 'GRChombo':LoadGRChomboData}
+import sys
 
 def LoadUliMode(filename):
     dat = np.genfromtxt(filename)
@@ -16,5 +15,44 @@ def LoadUliData(l, m, rex, inputdir, root='Psi4_vars'):
   filename = os.path.join(inputdir, root+'_l'+str(l)+'_m'+str(m)+'_rex'+str(rex).zfill(2))
   LoadUliMode(filename)
 
+def LoadUliParams(inputdir, paramfile=None):
+  '''Load simulation parameters from Uli's param file'''
+  if paramfile is None:
+    # convention is using parent directory name as root for parameter filename
+    paramfile = os.path.splitdrive(inputdir)[-1]+'.par'
+
+  filename = os.path.join(inputdir, paramfile)
+  return filename
+  
+def ListUliModes(inputdir):
+  '''Populate list of available modes for Psi4 from Uli's data directory'''
+  flist = os.listdir(loc)
+  modes = []
+  for f in flist:
+    fdec = f.split('_')
+    if fdec[0]+'_'+fdec[1] == 'Psi4_vars':
+      l=fdec[2][1:]
+      m=fdec[3][1:]
+      reslist.append[(l,m)]
+  return modes
+
+def LoadGRChomboParams(inputdir, paramfile=None):
+  '''Load simulation parameters from GRChombo param file'''
+  return
+
+def LoadGRChomboData(l, m, rex, inputdir, root='Psi4_vars'):
+  '''Load NR data for Psi4(t) from GRChombo output file'''
+  filename = os.path.join(inputdir, root+'_l'+str(l)+'_m'+str(m)+'_rex'+str(rex).zfill(2))
+  sys.exit(-1)
+
+def ListGRChomboModes(inputdir):
+  '''Populate list of available modes for Psi4 from GRChombo data directory'''
+  return
+
+
+
+loadDataFDict = {'Uli':LoadUliData, 'GRChombo':LoadGRChomboData}
+loadParamsFDict = {'Uli':LoadUliParams, 'GRChombo':LoadGRChomboParams}
+listModesFDict = {'Uli':ListUliModes, 'GRChombo':ListGRChomboModes}
 
 
